f7dae0f8a3613d4bb92f611fa1c3485041d49e0c
Tamara Papkova <papkova.td@gmail.com>
2025-09-14 14:03:18 +0300

    Added homework Datatypes  lesson04

---
+++ src/main/org.example.kotlin/org/example/kotlin/lessons/lesson04/homework/Datatypes.kt
@@ -0,0 +1,82 @@
+package org.example.org.example.kotlin.lessons.lesson04.homework
+
+class Datatypes {
+    val v1: Byte = 42
+    val v2: Long = 98765432123456789L
+    val v3: Float = 23.45f
+    val v4: Double = 0.123456789
+    val v5: String = "Kotlin & Java"
+    // v6 булевые значения нельзя использовать в верхнем регистре false
+    val v6: = FALSE
+    val v7: Char = 'c'
+    val v8: Short = 500
+    val v9: Long = 4294967296L
+    val v10: Float = 18.0f
+    val v11: Double = -0.001
+    val v12: String = "OpenAI"
+    // v13 булевое значение должно быть без кавычек true
+    val v13 = "true"
+    val v14: List<Byte> = listOf(3, 14)
+    val v15: Char = '9'
+    val v16: Short = 2048
+    val v17: Long = 10000000000L
+    val v18: Set<String> = setOf("OpenAI", "Quantum Computing")
+    val v19: Float = 5.75f
+    // v20 char использует одинарные кавчки и там может быть только 1 символ
+    val v20 = `1.414`
+    val v21: String = "Artificial Intelligence"
+    val v22: Array<Any> = arrayOf('x', "A")
+    val v23: String = "Android Studio"
+    val v24: Char = '@'
+    val v25: Short = 1024
+    val v26: Long = 1234567890123L
+    val v27: Float = 10.01f
+    val v28: Double = -273.15
+    val v29: String = "SpaceX"
+    // v30 булевые значения нельзя использовать в верхнем регистре false
+    val v30 = FALSE
+    val v31: Double = 0.007
+    // v32 char не может хранить эмодзи, нужно использовать String
+    val v32 = “🤯”
+    val v33: Map<String, Byte> = mapOf("true" to 2, "false" to 34)
+    // v34 одинарные кавычки обозначают char, где должен быть один символ
+    val v34 = ‘65535’
+    val v35: Long = 72057594037927935L
+    val v36: Float = 2.71828f
+    val v37: Double = 101.0101
+    val v38: String = "Quantum Computing"
+    val v39: Map<Int, String> = mapOf(2 to "true", 34 to "false")
+    val v40: Char = 'x'
+    val v41: Short = 314
+    val v42: Long = 123456789123456789L
+    val v43: Float = 6.626f
+    // v44 булевые значения нельзя использовать в верхнем регистре false
+    val v44 = TRUE
+
+
+    // Хранить букву, на которую указывает палец медиума во время спиритического сеанса.
+    val s1: Char = 'u'
+    // Хранить количество ложек сахара, которые я кладу в одну чашку чая.
+    val s2: Byte = 10
+    // Хранить список расходов на доставку еды, чтобы ещё раз убедиться, что готовить было дешевле.
+    val s3: Set<Double> = setOf(101.5, 25.8)
+    // Хранить длину очереди в столовой до миллиардной доли сантиметра.
+    val s4: Double = 500.5697
+    // Хранить факт, закрыт ли баг после того, как его просто переименовали в фичу.
+    val s5: Boolean = true
+    // Хранить количество свистков чайника за день.
+    val s6: Byte = 15
+    // Хранить количество нажатий котом на клавиатуру ноутбука за всё время твоей работы.
+    val s7: Byte = 100
+    // Хранить количество попыток пересчитать звёзды на небе за всю историю человечества.
+    val s8: Long = 1526446396515522536L
+    // Хранить массу воздуха в спускающем матрасе после нападения кота в долях грамма.
+    val s9: Double = 125.569
+    // Хранить словарь «название стартапа → сумму потерь инвесторов».
+    val s10: Map<String, Double> = mapOf("Стартап 1" to 154.55, "Стартап 2" to 555.896)
+    // Хранить строку «починилось само», чтобы закрывать тикеты без лишних слов.
+    val s11: String = "починилось само"
+    // Хранить список тем для митингов, которые на самом деле никому не нужны.
+    val s12: List<String> = listOf("Ненужная тема 1", "Ненужная тема 2")
+
+}
